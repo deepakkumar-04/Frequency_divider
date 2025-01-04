6eab437626c90e0c34a486c90e670b3434888262
deepakkumar-04 <rdeepakkumar2022@gmail.com>
2025-01-04 11:45:18 +0530

    Add files via upload

---
+++ frequency_divider_tb.v
@@ -0,0 +1,26 @@
+
+module frequency_divider_tb;
+    reg clk;                   
+    reg rst;                   
+    wire out;                  
+    freq_divider_by_odd #(5) uut (
+        .clk(clk),
+        .rst(rst),
+        .out(out)
+    );
+    always begin
+        #5 clk = ~clk;  
+    end    
+    initial begin
+        clk = 0;
+        rst = 0;
+        rst = 1;
+        #10;
+        rst = 0;
+        #100;
+        $finish;
+    end
+    initial begin
+        $monitor("Time = %0t, clk = %b, rst = %b, out = %b", $time, clk, rst, out);
+    end
+endmodule
